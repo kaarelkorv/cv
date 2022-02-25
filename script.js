efcbb2edcccd5ef365525a6ca120bf762043a34d
kaarelk <korv.kaarel@gmail.com>
2022-02-25 08:35:11 +0200

    first

---
+++ script.js
@@ -0,0 +1,33 @@
+let photoBoxImages = document.querySelectorAll("[data-image]")
+console.log()
+
+
+photoBoxImages.forEach(image => {
+    image.addEventListener("click", ()=>{
+               let img = image.parentElement.childNodes[2].childNodes
+                img.forEach(extra => {
+                    extra.style.opacity = "1"
+                    extra.style.zIndex = "3"
+                    extra.style.filter = "grayscale(0)";
+                    extra.style.filter = "grayscale(0)";
+                })
+
+    }) 
+
+    
+        let extras = image.parentElement.childNodes[2].childNodes
+        extras.forEach(extra => {
+            extra.addEventListener("click", (e)=>{
+                extra.style.opacity = "0"
+                extra.style.zIndex = "0"
+                
+                
+            })
+            extra.addEventListener("pointerleave", (e)=>{
+                extra.style.filter = "grayscale(1)";
+            })
+            extra.addEventListener("pointerenter", (e)=>{
+                extra.style.filter = "grayscale(0)";
+            })
+        })
+})
